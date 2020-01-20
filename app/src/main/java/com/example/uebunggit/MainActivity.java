aa35a4885bf98601c8728ed901e1998c702bcfc0
mmalekmichael <mmalekmichael@gmail.com>
2020-01-20 15:53:33 +0100

    Skrt

--- app/src/main/java/com/example/uebunggit/MainActivity.java
+++ app/src/main/java/com/example/uebunggit/MainActivity.java
@@ -2,13 +2,78 @@ package com.example.uebunggit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    private static LocationManager locMan = null;
+    TextView la;
+    TextView lo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locMan = (LocationManager) getSystemService(LOCATION_SERVICE);
+        la = findViewById(R.id.textView1);
+        lo = findViewById(R.id.textView2);
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        if (location == null) return;
+        displayLocation(location);
+
+    }
+
+    private void displayLocation(Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        la.setText(String.format("%.4f", latitude));
+        lo.setText(String.format("%.4f", longitude));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
     }
 }
