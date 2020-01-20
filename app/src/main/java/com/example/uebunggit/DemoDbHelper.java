93f2a2eff864eb1e97ff76ae645281f39e2c01ea
akymz <akaymaz44@gmail.com>
2020-01-20 14:38:50 +0100

    hhhh

--- app/src/main/java/com/example/uebunggit/DemoDbHelper.java
+++ app/src/main/java/com/example/uebunggit/DemoDbHelper.java
@@ -1,4 +1,26 @@
 package com.example.uebunggit;
 
-public class DemoDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DemoDbHelper extends SQLiteOpenHelper {
+
+    private final static String DB_NAME="tracking.db";
+    private final static int DB_VERSION=1;
+
+    public DemoDbHelper(Context context) {
+        super(context,DB_NAME,null,DB_VERSION);
+    }
+
+
+     @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
