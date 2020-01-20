a2a9af8b24aa2b615d49cca44edca20a8bdf618f
mmalekmichael <mmalekmichael@gmail.com>
2020-01-20 15:56:17 +0100

    Skrt

--- app/src/main/java/com/example/uebunggit/MainActivity.java
+++ app/src/main/java/com/example/uebunggit/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     TextView la;
     TextView lo;
 
+    //ff
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
